99143391e46c294824aadbf18cc607f70e5645df
vinzenzu <125748259+vinzenzu@users.noreply.github.com>
2024-03-06 20:58:46 +0100

    Simplify functions, add main

--- src/rag_chat.py
+++ src/rag_chat.py
@@ -1,22 +1,22 @@
 #!pip install --quiet langchain_community tiktoken langchain-openai langchainhub chromadb langchain langgraph tavily-python langchain-mistralai gpt4all
 
+import pprint
+
+import os
+
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import DirectoryLoader
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import GPT4AllEmbeddings
+from langchain_community.chat_models import ChatOllama
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.prompts import PromptTemplate
 
-import os
 
-# Embed and index
-embedding = GPT4AllEmbeddings()
-use_llm = "mistral:instruct"
-vectorstore = None
-
-print("---STARTING PROGRAM---")
-
-if not os.path.isdir('../data'):
+def create_vector_store(db_directory, data_directory, embedding):
     print("---CREATING VECTOR STORE (this may take a while)---")
-    loader = DirectoryLoader('../data')#, glob="**/*.txt")
+    loader = DirectoryLoader(data_directory)  # , glob="**/*.txt")
     docs = loader.load()
 
     # Split
@@ -30,290 +30,57 @@ if not os.path.isdir('../data'):
         documents=all_splits,
         collection_name="rag-chroma",
         embedding=embedding,
-        persist_directory="./chroma_db"
+        persist_directory=db_directory
     )
-else:
+    return vectorstore.as_retriever()
+
+
+def fetch_vector_store(db_directory, embedding):
     print("---FETCHING VECTOR STORE---")
-    vectorstore = Chroma(persist_directory="./chroma_db", embedding_function=embedding)
-
-retriever = vectorstore.as_retriever()
-
-from typing import Annotated, Dict, TypedDict
+    vectorstore = Chroma(persist_directory=db_directory, embedding_function=embedding)
+    return vectorstore.as_retriever()
 
 
-class GraphState(TypedDict):
-    """
-    Represents the state of our graph.
-
-    Attributes:
-        keys: A dictionary where each key is a string.
-    """
-
-    keys: Dict[str, any]
-
-from langchain import hub
-from langchain_core.output_parsers import JsonOutputParser
-from langchain.prompts import PromptTemplate
-from langchain.schema import Document
-from langchain_community.chat_models import ChatOllama
-from langchain_community.tools.tavily_search import TavilySearchResults
-from langchain_core.output_parsers import StrOutputParser
-
-### Nodes ###
-
-def retrieve(state):
-    """
-    Retrieve documents
-
-    Args:
-        state (dict): The current graph state
-
-    Returns:
-        state (dict): New key added to state, documents, that contains retrieved documents
-    """
+def retrieve(question):
     print("---RETRIEVE---")
-    state_dict = state["keys"]
-    question = state_dict["question"]
-    #local = state_dict["local"]
     documents = retriever.get_relevant_documents(question)
-    return {"keys": {"documents": documents, "question": question}}
+    return documents
 
 
-def generate(state):
-    """
-    Generate answer
-
-    Args:
-        state (dict): The current graph state
-
-    Returns:
-        state (dict): New key added to state, generation, that contains generation
-    """
+def generate(question, documents, use_llm):
     print("---GENERATE---")
-    state_dict = state["keys"]
-    question = state_dict["question"]
-    documents = state_dict["documents"]
+    # adapted from https://smith.langchain.com/hub/rlm/rag-prompt
+    rag_prompt = ChatPromptTemplate.from_template("You are an assistant for question-answering tasks. Use the "
+                                                  "following pieces of retrieved context to answer the question. If "
+                                                  "you don't know the answer, just say that you don't know. Keep the "
+                                                  "answer concise, truthful, and informative.\n"
+                                                  "Question: {question} \n"
+                                                  "Context: {context} \n"
+                                                  "Answer:")
 
-    # Prompt
-    prompt = hub.pull("rlm/rag-prompt")
-
-    # LLM
     llm = ChatOllama(model=use_llm, temperature=0)
-
-    # Post-processing
-    def format_docs(docs):
-        return "\n\n".join(doc.page_content for doc in docs)
-
-    # Chain
-    rag_chain = prompt | llm | StrOutputParser()
-
-    # Run
-    generation = rag_chain.invoke({"context": documents, "question": question})
-    return {
-        "keys": {"documents": documents, "question": question, "generation": generation}
-    }
+    chain = rag_prompt | llm | StrOutputParser()
+    output = chain.invoke({"context": documents, "question": question})
+    return output
 
 
-def grade_documents(state):
-    """
-    Determines whether the retrieved documents are relevant to the question.
-
-    Args:
-        state (dict): The current graph state
-
-    Returns:
-        state (dict): Updates documents key with relevant documents
-    """
-
-    print("---CHECK RELEVANCE---")
-    state_dict = state["keys"]
-    question = state_dict["question"]
-    documents = state_dict["documents"]
-
-    # LLM
-    llm = ChatOllama(model=use_llm, format="json", temperature=0)
-
-    prompt = PromptTemplate(
-        template="""You are a grader assessing relevance of a retrieved document to a user question. \n 
-        Here is the retrieved document: \n\n {context} \n\n
-        Here is the user question: {question} \n
-        If the document contains keywords related to the user question, grade it as relevant. \n
-        It does not need to be a stringent test. The goal is to filter out erroneous retrievals. \n
-        Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question. \n
-        Provide the binary score as a JSON with a single key 'score' and no premable or explaination.""",
-        input_variables=["question", "context"],
-    )
-
-    chain = prompt | llm | JsonOutputParser()
-
-    # Score
-    filtered_docs = []
-    search = "No"  # Default do not opt for web search to supplement retrieval
-    for d in documents:
-        score = chain.invoke(
-            {
-                "question": question,
-                "context": d.page_content,
-            }
-        )
-        grade = score["score"]
-        if grade == "yes":
-            print("---GRADE: DOCUMENT RELEVANT---")
-            filtered_docs.append(d)
-        else:
-            print("---GRADE: DOCUMENT NOT RELEVANT---")
-            search = "Yes"  # Perform web search
-            continue
-
-    return {
-        "keys": {
-            "documents": filtered_docs,
-            "question": question,
-            "run_web_search": search,
-        }
-    }
-
-
-def transform_query(state):
-    """
-    Transform the query to produce a better question.
-
-    Args:
-        state (dict): The current graph state
-
-    Returns:
-        state (dict): Updates question key with a re-phrased question
-    """
-
-    print("---TRANSFORM QUERY---")
-    state_dict = state["keys"]
-    question = state_dict["question"]
-    documents = state_dict["documents"]
-
-    # Create a prompt template with format instructions and the query
-    prompt = PromptTemplate(
-        template="""You are generating questions that is well optimized for retrieval. \n 
-        Look at the input and try to reason about the underlying sematic intent / meaning. \n 
-        Here is the initial question:
-        \n ------- \n
-        {question} 
-        \n ------- \n
-        Provide an improved question without any premable, only respond with the updated question: """,
-        input_variables=["question"],
-    )
-
-    # Grader
-    # LLM
-    llm = ChatOllama(model=use_llm, temperature=0)
-
-    # Prompt
-    chain = prompt | llm | StrOutputParser()
-    better_question = chain.invoke({"question": question})
-
-    return {
-        "keys": {"documents": documents, "question": better_question}
-    }
-
-
-def web_search(state):
-    """
-    Web search based on the re-phrased question using Tavily API.
-
-    Args:
-        state (dict): The current graph state
-
-    Returns:
-        state (dict): Web results appended to documents.
-    """
-
-    print("---WEB SEARCH---")
-    state_dict = state["keys"]
-    question = state_dict["question"]
-    documents = state_dict["documents"]
-
-    tool = TavilySearchResults()
-    docs = tool.invoke({"query": question})
-    web_results = "\n".join([d["content"] for d in docs])
-    web_results = Document(page_content=web_results)
-    documents.append(web_results)
-
-    return {"keys": {"documents": documents, "question": question}}
-
-
-### Edges
-
-
-def decide_to_generate(state):
-    """
-    Determines whether to generate an answer or re-generate a question for web search.
-
-    Args:
-        state (dict): The current state of the agent, including all keys.
-
-    Returns:
-        str: Next node to call
-    """
-
-    print("---DECIDE TO GENERATE---")
-    state_dict = state["keys"]
-    question = state_dict["question"]
-    filtered_documents = state_dict["documents"]
-    search = state_dict["run_web_search"]
-
-    if search == "Yes":
-        # All documents have been filtered check_relevance
-        # We will re-generate a new query
-        print("---DECISION: TRANSFORM QUERY and RUN WEB SEARCH---")
-        return "transform_query"
+if __name__ == "__main__":
+    print("---STARTING PROGRAM---")
+    use_llm = "mistral:instruct"
+    embedding = GPT4AllEmbeddings()
+    data_directory = '../data'
+    db_directory = './chroma_db'
+    retriever = None
+    # creating or fetching vector store
+    if not os.path.isdir(db_directory):
+        retriever = create_vector_store(db_directory, data_directory, embedding)
     else:
-        # We have relevant documents, so generate answer
-        print("---DECISION: GENERATE---")
-        return "generate"
+        retriever = fetch_vector_store(db_directory, embedding)
 
-import pprint
+    input_question = "How do you compile regular expressions in Python?"
+    docs = retrieve(input_question)
+    output = generate(input_question, docs, use_llm)
 
-from langgraph.graph import END, StateGraph
+    #pprint.pprint(output)
 
-workflow = StateGraph(GraphState)
-
-# Define the nodes
-workflow.add_node("retrieve", retrieve)  # retrieve
-workflow.add_node("grade_documents", grade_documents)  # grade documents
-workflow.add_node("generate", generate)  # generatae
-workflow.add_node("transform_query", transform_query)  # transform_query
-workflow.add_node("web_search", web_search)  # web search
-
-# Build graph
-workflow.set_entry_point("retrieve")
-workflow.add_edge("retrieve", "grade_documents")
-workflow.add_conditional_edges(
-    "grade_documents",
-    decide_to_generate,
-    {
-        "transform_query": "transform_query",
-        "generate": "generate",
-    },
-)
-workflow.add_edge("transform_query", "web_search")
-workflow.add_edge("web_search", "generate")
-workflow.add_edge("generate", END)
-
-# Compile
-app = workflow.compile()
-
-# Run
-inputs = {
-    "keys": {
-        "question": "How do you compile regular expressions in Python?" #"Explain how the different types of agent memory work?",
-    }
-}
-for output in app.stream(inputs):
-    for key, value in output.items():
-        # Node
-        pprint.pprint(f"Node '{key}':")
-        # Optional: print full state at each node
-        # pprint.pprint(value["keys"], indent=2, width=80, depth=None)
-    pprint.pprint("\n---\n")
-
-# Final generation
-pprint.pprint(value["keys"]["generation"])
+    print(output)
